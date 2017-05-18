83e6664c6c2a0f62b3120f2b32736aceb3e2f6fc
pqpredu <pcn2009@hotmail.com>
2017-05-18 17:30:02 -0300

    Add files via upload

---
+++ main4.js
@@ -0,0 +1,24 @@
+var alfa = function(){console.log('alfa')}
+beta = ()=> {console.log('beta')}
+function gama(){console.log('gama')}
+var somar = ( a , b) => { return(a + b) }
+console.log ( a + b )
+alfa()
+beta()
+gama()
+console.log( somar(2,12) )
+var quadrado = (numero) => { return(a * b) }
+
+var quad_de_3 = quadrado (3)
+console.log(quad_de_3)
+{
+primeira:null,
+soma:( a, b )=>{ return(a + b)},
+subtrair:( a, b )=>{return(a - b)},
+dividir:( a, b )=>{return(a / b)},
+multiplicar:( a, b )=>{return(a * b)}
+}
+console.log(matematica.somar(12,3) )
+console.log(matematica.subtrair(12,3) )
+console.log(matematica.dividir(12,3) )
+console.log(matematica.multiplicar(12,3) )

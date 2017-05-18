ff7833448be2834e046cd7cb7f0821b159788d42
pqpredu <pcn2009@hotmail.com>
2017-05-18 16:54:42 -0300

    Update main3.js

--- main3.js
+++ main3.js
@@ -8,3 +8,8 @@ console.log(objeto.alfa)
 var valor = objeto['banana'] + objeto.nitro
 console.log(valor)
 objeto.nov = 13 + 12/4 //PEMDAS
+console.log(objeto)
+console.maria = 'abacaxi' //adiciona objeto
+console.log(objeto)
+delete objeto['maria'] // remover o objeto
+console.log(objeto)

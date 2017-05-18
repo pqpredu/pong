d0a901cac07c746f3ea4c455ddb2ea55c254fe0e
pqpredu <pcn2009@hotmail.com>
2017-05-18 16:55:36 -0300

    Update main3.js

--- main3.js
+++ main3.js
@@ -7,9 +7,9 @@ nitro:3
 console.log(objeto.alfa)
 var valor = objeto['banana'] + objeto.nitro
 console.log(valor)
-objeto.nov = 13 + 12/4 //PEMDAS
+
 console.log(objeto)
-console.maria = 'abacaxi' //adiciona objeto
+objeto['maria'] = 'abacaxi' //adiciona objeto
 console.log(objeto)
 delete objeto['maria'] // remover o objeto
 console.log(objeto)
